2f1ece1651a36a09e7fbe786ff89f794819b8ec4
Enes52001 <enes.sahin@student.hu.nl>
2021-06-24 10:46:01 +0200

    CI build slaagt nu wel

---
+++ src/main/java/nl/hu/cisq1/lingo/trainer/data/SpringGameRepository.java
@@ -0,0 +1,16 @@
+package nl.hu.cisq1.lingo.trainer.data;
+
+import nl.hu.cisq1.lingo.trainer.domain.Game;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.List;
+
+public interface SpringGameRepository extends JpaRepository<Game, Long> {
+    @Override
+    default List<Game> findAllById(Iterable<Long> iterable) {
+        return null;
+    }
+
+
+
+}
